2992fccf42210d0614a37f876e2adb1f2337d2b6
vainawebuser <vainawebcpx@gmail.com>
2018-01-20 15:38:01 -0200

    desafiocissa

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <section>
+      <h1>Desafio</h1>
+      <p>Vai na web EAD</p>
+      <img src="1.jpg">
+    </section>
+      <div>
+        <ul>
+          <li>inicio</li>
+          <li>sobre</li>
+          <li>contato</li>
+        </ul>
+      </div>
+      <div>
+        <p>O vai na web é um curso de desenvolvimento de páginas web e cidadania dentro de organizações da base comunitária para jovens de 16 a 29 anos  que estão  iniciando a carreira e querem aprender fazendo.</p>
+        <img src="../assets/wnw.jpg" alt="Um dos polos do programa">
+      </div>
+    
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
